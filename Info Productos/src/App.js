fa195549ef3cdb11dfff3c88b2fd8797e68dbcb2
Kevin Bohorquez <90785399+ASCksbl@users.noreply.github.com>
2021-10-03 16:05:36 -0500

    Add files via upload

---
+++ Info Productos/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductReg from './Components/See Products/ProductApp'
+
+function App() {
+  return (
+    <div>
+      <ProductReg />
+    </div>
+  )
+}
+
+export default App
