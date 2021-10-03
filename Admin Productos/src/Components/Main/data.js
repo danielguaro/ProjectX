7aba59ebc3b6b6a6f2a07062dc8f9c46ad480cb8
Kevin Bohorquez <90785399+ASCksbl@users.noreply.github.com>
2021-10-03 16:27:46 -0500

    Create data.js

---
+++ Admin Productos/src/Components/Main/data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: 1,
+    productName: 'Chair',
+    description: 'Is for you to sit',
+    unitValue: '$ 10',
+    state: 'available',
+  },
+  {
+    id: 2,
+    productName: 'Computer',
+    description: 'Is for you to do tecnologic stuff',
+    unitValue: '$ 100',
+    state: 'available',
+  },
+  {
+    id: 3,
+    productName: 'Television',
+    description: 'Is for you to watch netflix',
+    unitValue: '$ 90',
+    state: 'not available',
+  },
+  {
+    id: 4,
+    productName: 'Bed',
+    description: 'Is for you to sleep',
+    unitValue: '$ 50',
+    state: 'available',
+  },
+]
+
+export default data
