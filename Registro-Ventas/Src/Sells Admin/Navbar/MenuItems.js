aa552bb5ce2e50fa974fe6adee04fbd4eb231379
DanielRey24 <90940417+DanielRey24@users.noreply.github.com>
2021-10-03 16:15:40 -0500

    Add files via upload

---
+++ Registro-Ventas/Src/Sells Admin/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Sells Admin',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Product Info',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'User Master',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Log Out',
+    url: '#',
+    cName: 'nav-links-mobile',
+  },
+]
