b042654dbbd51e4f0de2f478426522766bac2854
Kevin Bohorquez <90785399+ASCksbl@users.noreply.github.com>
2021-10-03 16:10:43 -0500

    Add files via upload

---
+++ Info Productos/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Sells Info',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Product Admin',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'User Master',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Log Out',
+    url: '#',
+    cName: 'nav-links-mobile',
+  },
+]
