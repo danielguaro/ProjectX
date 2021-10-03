f6c7d0218e2bf6b79933407c135c882067ea65ab
Kevin Bohorquez <90785399+ASCksbl@users.noreply.github.com>
2021-10-03 16:33:35 -0500

    Create main.js

---
+++ Home Page/src/Components/Main/main.js
@@ -0,0 +1,22 @@
+import Fondo from '../Images/Fondo.jpg'
+
+export const Main = () => {
+    return (
+      <>
+        <main>
+          <div class='main_section'>
+            <h1>SD Ascension</h1>
+            <div class='underline'></div>
+            <h2>
+              <p>Welcome to the company page.</p>
+              <hr></hr>
+              <p>To Log in the system press the next button</p>
+            </h2>
+            <a>
+              <button>Log in</button>
+            </a>
+          </div>
+        </main>
+      </>
+    )
+}
