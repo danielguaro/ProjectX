fcd1f3963bb42c246a5c50130bc3e427e8e6635f
Kevin Bohorquez <90785399+ASCksbl@users.noreply.github.com>
2021-10-03 16:37:54 -0500

    Create MenuItems.js

---
+++ Home Page/src/Components/Navbar1/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Log in',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
