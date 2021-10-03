c210eb633d892627ddbf68604dfa02d35eb51a42
Kevin Bohorquez <90785399+ASCksbl@users.noreply.github.com>
2021-10-03 16:32:48 -0500

    Add files via upload

---
+++ Home Page/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Components/Navbar1/Navbar'
+import {Main} from './Components/Main/main'
+import {Footer} from './Components/Footer/footer'
+
+function App(){
+
+    return<>
+    <header>
+        <Navbar />
+        <Main />
+        <Footer />
+    </header>
+    </>
+}
+
+
+
+
+
+
+export default App;
