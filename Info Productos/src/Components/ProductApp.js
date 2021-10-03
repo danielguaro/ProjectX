38d18db2db798c9054e11efd50b0101587591758
Kevin Bohorquez <90785399+ASCksbl@users.noreply.github.com>
2021-10-03 16:06:39 -0500

    Create ProductApp.js

---
+++ Info Productos/src/Components/ProductApp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Products from './Main/main'
+
+function Product() {
+  return (
+    <div>
+      <Navbar />
+      <Products />
+    </div>
+  )
+}
+
+export default Product
