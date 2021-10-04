99cda331d16bf662e3469dbb60d7e9afbc456a8f
Camilozv94 <90917902+Camilozv94@users.noreply.github.com>
2021-10-03 19:58:03 -0500

    items de main añadidos

---
+++ Admin Ventas/src/Sells Admin/Main/data.js
@@ -0,0 +1,52 @@
+const data = [
+  {
+    id: 1,
+    valorTotal: '$1000',
+    idProducto: 1,
+    cantidad: 10,
+    precio: '$100',
+    fechaVenta: '20/06/2021',
+    idDocumento: 10098279,
+    cliente: 'Carlos',
+    vendedor: 'Jose',
+    estado: 'entregado'
+  },
+  {
+    id: 2,
+    valorTotal: '$1200',
+    idProducto: 2,
+    cantidad: 8,
+    precio: '$150',
+    fechaVenta: '16/05/2021',
+    idDocumento: 10078021,
+    cliente: 'Valentina',
+    vendedor: 'Jose',
+    estado: 'proceso'
+  },
+  {
+    id: 3,
+    valorTotal: '$700',
+    idProducto: 3,
+    cantidad: 14,
+    precio: '$50',
+    fechaVenta: '01/01/2021',
+    idDocumento: 101738291,
+    cliente: 'Victor',
+    vendedor: 'Daniel',
+    estado: 'entregado'
+  },
+  {
+    id: 4,
+    valorTotal: '$500',
+    idProducto: 4,
+    cantidad: 1,
+    precio: '$500',
+    fechaVenta: '01/10/2021',
+    idDocumento: 100567832,
+    cliente: 'Camilo',
+    vendedor: 'Kevin',
+    estado: 'cancelado'
+  },
+]
+
+export default data
