d36d6855d9e8fa2808b5da643fcea943b1e42567
Camilozv94 <90917902+Camilozv94@users.noreply.github.com>
2021-10-03 19:59:34 -0500

    aplicación que contiene el main y navbar

---
+++ Admin Ventas/src/Sells Admin/Venta.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Ventas from './Main/main'
+
+function Venta() {
+  return (
+    <div>
+      <Navbar />
+      <Ventas />
+    </div>
+  )
+}
+
+export default Venta
